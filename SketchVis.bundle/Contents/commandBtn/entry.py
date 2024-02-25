9083debbb34cbc2e23988f0411af79c08576c2ce
enly1 <enly1@techlib.co.uk>
2024-02-25 17:04:49 +0000

    Initial SketchVis Published Add-in

---
+++ SketchVis.bundle/Contents/commandBtn/entry.py
@@ -0,0 +1,91 @@
+import adsk.core
+import os
+from ..lib import fusion360utils as futil
+from .. import config
+app = adsk.core.Application.get()
+ui = app.userInterface
+
+
+# TODO *** Specify the command identity information. ***
+CMD_ID = f'{config.COMPANY_NAME}_{config.ADDIN_NAME}_cmdBtn'
+CMD_NAME = 'SketchVis'
+CMD_Description = 'Toggle Display Settings | Object Visibility | Sketches'
+
+# Specify that the command will be promoted to the panel.
+IS_PROMOTED = True
+
+# TODO *** Define the location where the command button will be created. ***
+# This is done by specifying the workspace, the tab, and the panel, and the 
+# command it will be inserted beside. Not providing the command to position it
+# will insert it at the end.
+WORKSPACE_ID = 'FusionSolidEnvironment'
+PANEL_ID = 'SolidScriptsAddinsPanel'
+COMMAND_BESIDE_ID = 'ScriptsManagerCommand'
+
+# Resource location for command icons, here we assume a sub folder in this directory named "resources".
+ICON_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', '')
+
+# Local list of event handlers used to maintain a reference so
+# they are not released and garbage collected.
+local_handlers = []
+
+
+# Executed when add-in is run.
+def start():
+    # Create a command Definition.
+    cmd_def = ui.commandDefinitions.addButtonDefinition(CMD_ID, CMD_NAME, CMD_Description, ICON_FOLDER)
+
+    # Define an event handler for the command created event. It will be called when the button is clicked.
+    futil.add_handler(cmd_def.commandCreated, command_created)
+
+    # ******** Add a button into the UI so the user can run the command. ********
+    # Get the target workspace the button will be created in.
+    workspace = ui.workspaces.itemById(WORKSPACE_ID)
+
+    # Get the panel the button will be created in.
+    panel = workspace.toolbarPanels.itemById(PANEL_ID)
+
+    # Create the button command control in the UI after the specified existing command.
+    control = panel.controls.addCommand(cmd_def, COMMAND_BESIDE_ID, False)
+
+    # Specify if the command is promoted to the main toolbar. 
+    control.isPromoted = IS_PROMOTED
+
+
+# Executed when add-in is stopped.
+def stop():
+    # Get the various UI elements for this command
+    workspace = ui.workspaces.itemById(WORKSPACE_ID)
+    panel = workspace.toolbarPanels.itemById(PANEL_ID)
+    command_control = panel.controls.itemById(CMD_ID)
+    command_definition = ui.commandDefinitions.itemById(CMD_ID)
+
+    # Delete the button command control
+    if command_control:
+        command_control.deleteMe()
+
+    # Delete the command definition
+    if command_definition:
+        command_definition.deleteMe()
+
+
+# Function that is called when a user clicks the corresponding button in the UI.
+# This defines the contents of the command dialog and connects to the command related events.
+def command_created(args: adsk.core.CommandCreatedEventArgs):
+
+    try:
+        app = adsk.core.Application.get()
+        ui = app.userInterface
+
+        control = ui.commandDefinitions.itemById('VisibilityOverrideCommand')
+        subcmd = control.controlDefinition
+        # Sketches Item (Only works until they add items before) - Can't use name as that is internationalised
+        sub = subcmd.listItems.item(8) 
+        
+        sub.isSelected = not sub.isSelected
+        futil.log(f'{CMD_NAME} Sketch visibility located and toggled.  Now {str(sub.isSelected)}')
+
+    except Exception as e:
+        if ui:
+            ui.messageBox('Sketch visibility Update Failed. Contact the add-in provider\n')
+
